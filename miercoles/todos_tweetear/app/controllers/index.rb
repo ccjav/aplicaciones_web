f78503a6799a1704a5f3abc192accb061b4c58db
codeaj <codea_mac_05@CodeaCamp05s-Mac-mini.local>
2017-03-01 17:19:01 -0600

    detalle en index.rb

--- miercoles/todos_tweetear/app/controllers/index.rb
+++ miercoles/todos_tweetear/app/controllers/index.rb
@@ -5,17 +5,5 @@ before '/' do
 end
 
 get '/' do
-  # session.clear
-  # puts "current user"
-  # p @current_user.oauth_token
-  # p $client.methods
-  p $client
-  # p "session es #{@session}"
-  # current_user
-  # p "current user es #{@current_user}"
-  # p @session
-  # p session
-  # p @current_user
-  # p ENV
   erb :index
 end
